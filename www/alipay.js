b67bb044dba2745de3b3a7f5ff5192c8eb0ed4cc
wtto00 <wtto00@msn.com>
2023-05-12 21:44:56 +0800

    fix: exports.ResultStatus

--- www/alipay.js
+++ www/alipay.js
@@ -1,5 +1,14 @@
-var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
-exports.payment = function(payInfo, success, error) {
-    exec(success, error, "alipay", "payment", [payInfo]);
+exports.payment = function (payInfo, success, error) {
+  exec(success, error, "alipay", "payment", [payInfo]);
+};
+
+exports.ResultStatus = {
+  9000: "订单处理成功",
+  8000: "正在处理中",
+  4000: "订单支付失败",
+  6001: "用户中途取消",
+  6002: "网络连接出错",
+  6004: "支付结果未知",
 };
